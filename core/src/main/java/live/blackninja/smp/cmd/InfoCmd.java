0f6be3b0eec91fe1e3c0d7db891b60243e8eae33
blackNinja46 <clemens.schulze@outlook.de>
2025-07-21 14:21:42 +0200

    Add InfoCmd to display server and addon status details
    Introduced a new command `InfoCmd` to provide players with server status, Minecraft version, and addon statuses in a formatted message. This command checks player permissions and uses `MessageBuilder` for consistent message formatting.

---
+++ core/src/main/java/live/blackninja/smp/cmd/InfoCmd.java
@@ -0,0 +1,57 @@
+package live.blackninja.smp.cmd;
+
+import live.blackninja.smp.Core;
+import live.blackninja.smp.builder.MessageBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class InfoCmd implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("ninjasmp.cmd.info")) {
+            player.sendMessage(MessageBuilder.buildOld(Core.NO_PERMS));
+            return false;
+        }
+
+        player.sendMessage(MessageBuilder.build(
+                "<dark_gray>*----------------------------------------------------*</dark_gray>\n" +
+                "\n" +
+                "<color:#4498db>SMP Core System</color> " + Bukkit.getVersion() + "\n" +
+                "\n" +
+                "<gray>Core Status</gray> <dark_gray>→</dark_gray> " + getStatusText("SMP") + "\n" +
+                "<gray>MC-Version</gray> <dark_gray>→</dark_gray> " + Bukkit.getMinecraftVersion() + "\n" +
+                "\n" +
+                "<color:#ffc800>Addons: </color>\n" +
+                "<dark_gray>-</dark_gray> <gray>Event</gray> <dark_gray>→</dark_gray> " + getStatusText("SMPEvent") + "\n" +
+                "<dark_gray>-</dark_gray> <gray>Webhook</gray> <dark_gray>→</dark_gray> " + getStatusText("SMPWebhook") + "\n" +
+                "<dark_gray>-</dark_gray> <gray>Economy</gray> <dark_gray>→</dark_gray> " + getStatusText("SMPEconomy") + "\n" +
+                "\n" +
+                "<dark_gray>*----------------------------------------------------*</dark_gray>"
+        ));
+
+        return false;
+    }
+
+    private boolean getStatus(String plugin) {
+        return Bukkit.getServer().getPluginManager().getPlugin(plugin) != null && Bukkit.getPluginManager().isPluginEnabled(plugin);
+    }
+
+    private String getStatusText(String plugin) {
+        if (getStatus(plugin)) {
+            return "<color:#00ff00>Online</color>";
+        } else {
+            return "<color:#ff0000>Offline</color>";
+        }
+    }
+}
+
